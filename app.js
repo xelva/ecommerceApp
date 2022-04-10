a475b4772968a6e48baa88eb2db8810193f34bd0
xelva <xelvacodes@gmail.com>
2022-04-10 16:35:37 -0500

    added post request for form submission

--- app.js
+++ app.js
@@ -8,14 +8,28 @@ app.get('/', (req, res)=>{
     //if response looks like html, express will send it back for the browser to interprit it as such
     res.send(`
     <div>
-        <form>
-            <input placeholder="email"/>
-            <input placeholder="password"/>
-            <input placeholder="password confirmation"/>
+        <form method="POST">
+            <input name="email" placeholder="email"/>
+            <input name="password" placeholder="password"/>
+            <input name="passwordConfirmation" placeholder="passwordConfirmation"/>
             <button>Sign Up</button>
         </form>
     </div>
     `);
+});
+
+app.post('/', (req, res) => {
+    //get access to the incoming info
+    req.on('data', data => {
+        const parsed = data.toString('utf8').split('&');//convert buffer to string and parse
+        const formData = {};
+        for (let pair of parsed) {
+            const [key, value] = pair.split('='); //destructure to assign first value of array to key, second to value
+            formData[key] = value;
+        }
+        console.log(formData);
+    })
+    res.send('Account created');
 });
 
 app.listen(3000, () => {
