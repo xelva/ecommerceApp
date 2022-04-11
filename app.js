fcf83ad0b610d97d40e0707564f754ed65cfd9df
xelva <xelvacodes@gmail.com>
2022-04-11 11:33:20 -0500

    finished with user auth

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const usersRepo = require('./repositories/users.js');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 
 
 const app = express(); //describes everything our web server can do
 app.use(bodyParser.urlencoded({ extended: true }));//use 'use' to have all route handlers use this middleware, will not be used in GET request
+app.use(cookieSession({
+    keys: ['asdflkjdsifjlsdf443jl']
+}));
 
-app.get('/', (req, res)=>{
+app.get('/signup', (req, res)=>{
     //req = request, incoming 
     //res = response, outgoing
     //if response looks like html, express will send it back for the browser to interprit it as such
     res.send(`
     <div>
+        Your id is: ${req.session.userId}
         <form method="POST">
             <input name="email" placeholder="email"/>
             <input name="password" placeholder="password"/>
@@ -23,7 +28,7 @@ app.get('/', (req, res)=>{
 });
 
 
-app.post('/', async (req, res) => { //add any middlewear functions before req,res param
+app.post('/signup', async (req, res) => { //add any middlewear functions before req,res param
     const { email, password, passwordConfirmation } = req.body;
     const existingUser = await usersRepo.getOneBy({ email });
     if (existingUser) {
@@ -33,8 +38,55 @@ app.post('/', async (req, res) => { //add any middlewear functions before req,re
         return res.send('Passwords must match');
     }
 
+    //create a user in rep 
+
+    const user = await usersRepo.create({ email, password});
+
+    //store the id and include in the user cookie
+    //cookie-session library adds middleware, allows us to access req.session
+    req.session.userId = user.id;
+
     res.send('Account created');
 });
+
+app.get('/signout', (req, res) => {
+    req.session = null;
+    res.send(`You're logged out!`)
+})
+
+app.get('/signin', (req, res) => {
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email"/>
+            <input name="password" placeholder="password"/>
+            <button>Sign In</button>
+        </form>
+    </div>
+    `)
+})
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    
+    const user = await (usersRepo.getOneBy( { email }));
+    if (!user) {
+        return res.send('Email not found');
+    }
+
+    const validPassword = await usersRepo.comparePassword(
+        user.password,
+        password
+    );
+
+    if (!validPassword) {
+        return res.send('Invalid password');
+    }
+
+    req.session.userId = user.id;
+
+    res.send('You are signed in!!');
+})
 
 app.listen(3000, () => {
     //setup your server to watch for incoming requests on the specified port (3000 in this case)
--- repositories/users.js
+++ repositories/users.js
@@ -1,6 +1,10 @@
 const { accessSync, constants, writeFileSync } = require('fs');
 const { readFile, writeFile } = require('fs/promises');
-const { randomBytes } = require('crypto');
+const { randomBytes, scrypt } = require('crypto');
+const util = require('util');
+
+const scryptPromise = util.promisify(scrypt);
+
 
 class UsersRepository {
     constructor(filename) {
@@ -22,10 +26,31 @@ class UsersRepository {
     }
     async create(attrs) {
         attrs.id = this.randomId();
+
+        const salt = randomBytes(8).toString('hex');
+        const buff = await scryptPromise(attrs.password, salt, 64)
+
         const records = await this.getAll();
-        records.push(attrs);
-        await this.writeAll(records);
         
+        const record = {
+            ...attrs, 
+            password: `${buff.toString('hex')}.${salt}`
+        };
+
+        records.push(record);
+
+        await this.writeAll(records);
+
+        return record;
+        
+    }
+
+    async comparePassword(saved, supplied){
+        //saved is pass saved in our database
+        //supplied is pass entered by user
+        const [hashed, salt] = saved.split('.');
+        const hashedSuppliedBuff = await scryptPromise(supplied, salt, 64);
+        return hashed === hashedSuppliedBuff.toString('hex');
     }
     async writeAll(records) {
         await writeFile(this.filename, JSON.stringify(records, null, 2)); // write file, all on new lines
@@ -78,5 +103,5 @@ class UsersRepository {
 //export class
 
 
-module.exports = new UsersRepository('users.json')
+module.exports = new UsersRepository('users.json');
 
