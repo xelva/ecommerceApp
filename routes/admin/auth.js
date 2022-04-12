b0ae39007e5afe6627c3b7ef0370c18686934c9d
xelva <xelvacodes@gmail.com>
2022-04-12 12:48:10 -0500

    updated form validation and sanitization

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const usersRepo = require('../../repositories/users.js');
 const signupTemplate = require('../../views/admin/auth/signup.js');
 const signinTemplate = require('../../views/admin/auth/signin.js')
+const { check, validationResult} = require('express-validator');
 
 //create a subrouter to export to the main express app router in app.js
 const router = express.Router();
@@ -14,16 +15,43 @@ router.get('/signup', (req, res)=>{
 });
 
 
-router.post('/signup', async (req, res) => { //add any middlewear functions before req,res param
-    const { email, password, passwordConfirmation } = req.body;
-    const existingUser = await usersRepo.getOneBy({ email });
-    if (existingUser) {
-        return res.send('Email in use');
-    }
+router.post('/signup', 
+    [
+        check('email')
+            .trim()
+            .normalizeEmail()
+            .isEmail()
+            
+            .custom(async email => {
+                    const existingUser = await usersRepo.getOneBy({ email });
+                    if (existingUser) {
+                        throw new Error('Email in use');
+                    }
+                }
+            ),
+        check('password')
+            .trim()
+            .isLength({min: 4, max: 20})
+            .withMessage('Must be between 4 and 20 characters'), 
+        check('passwordConfirmation')
+            .trim()
+            .isLength({min: 4, max: 20})
+            .withMessage('Must be between 4 and 20 characters')
+            .custom((passwordConfirmation, { req }) => {
+                if (passwordConfirmation !== req.body.password) {
+                    throw new Error('Passwords must match');
+                }
+            })
+    ],
+    async (req, res) => { //add any middlewear functions before req,res param
+        const errors = validationResult(req);
+        console.log(errors);
+
+        const { email, password, passwordConfirmation } = req.body;
+       
     if (password !== passwordConfirmation) {
         return res.send('Passwords must match');
     }
-
     //create a user in rep 
 
     const user = await usersRepo.create({ email, password});
