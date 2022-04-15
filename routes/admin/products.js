11e333befd883448fef8c2f84b797f5a0293ce55
xelva <xelvacodes@gmail.com>
2022-04-15 17:03:27 -0500

    adding ability to edit products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -5,8 +5,10 @@ const multer = require('multer');
 const { handleErrors, requireAuth } = require('./middlewares.js')
 const productsNewTemplate = require('../../Views/admin/products/new.js');
 const productsIndexTemplate = require('../../Views/admin/products/index.js')
+const productsEditTemplate = require('../../Views/admin/products/edit.js')
 const { checkTitle, checkPrice } = require('./validators.js');
 const productsRepo = require('../../repositories/products.js')
+
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -29,6 +31,35 @@ router.post('/admin/products/new', requireAuth, upload.single('image'), [checkTi
     const { title, price } = req.body;
     await productsRepo.create({title, price, image});
     res.redirect('/admin/products'); //redirect instead of just sending them message
+});
+
+//use :id to take in any string of characters into the string
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+    const product = await productsRepo.getOne(req.params.id);
+    if (!product) {
+        return res.send('Product not found');
+    }
+    res.send(productsEditTemplate({ product }));
+
+}); 
+
+router.post('/admin/products/:id/edit', requireAuth,
+    upload.single('image'),
+    [checkTitle, checkPrice],
+    handleErrors(productsEditTemplate),
+    async (req, res) => {
+        const changes = req.body;
+
+        if (req.file) {
+            changes.image = req.file.buffer.toString('base64');
+        }
+        try {
+            await productsRepo.update(req.params.id, changes)
+        } catch (err) {
+            return res.send('Could not find item');
+        }
+        res.redirect('/admin/products');
+
 });
 
 module.exports = router;
